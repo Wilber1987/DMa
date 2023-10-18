48bce4a78f4b61d0e84ad53a8ef1aa6e1fd80e1c
Kenelma93 <kenelmag93@gmail.com>
2023-10-18 16:47:32 -0600

    img

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ import { Home } from "./js/views/Home.js"
 window.onload = ()=>{
     AppMain.append(new Home())
 }
+import { Banner } from "./js/views/Banner.js"
+
+window.onload = ()=>{
+    AppMain.append(new Banner())
+}
---
+++ js/views/Banner.js
@@ -0,0 +1,27 @@
+
+const headerData = [
+    {
+      path: 'home',
+      label: 'Conócenos',
+    },
+    
+    {
+      path: 'team-section',
+      label: 'Docentes',
+    },
+    {
+      path: 'blog-section',
+      label: 'Contenido de clases',
+    },
+    {
+      path: 'package',
+      label: 'Kit Plan',
+    },
+    //{
+     // path: 'values',
+    //  label: 'Valores',
+    //},
+  ]
+  
+  export default headerData
+  
