d90f43870714e73b36054f600332d5886ba44bd8
Kenelma93 <kenelmag93@gmail.com>
2023-10-21 16:10:08 -0600

    prueba

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import { Home } from "./js/views/Home.js"
-import Footer from "./js/views/footer.js"
-import headerData from "./js/views/header.data.js"
-import Logo from "./js/views/logo.js"
+import {Footer} from "./js/views/footer.js"
+import {headerData} from "./js/views/header.data.js"
+import {Logo} from "./js/views/logo.js"
+
 
 window.onload = ()=>{
     AppMain.append(new Home())
