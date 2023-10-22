9e36da1c80eb96eeb953226ebe1ba2c79f2b515b
Kenelma93 <kenelmag93@gmail.com>
2023-10-21 22:49:25 -0600

    cambios

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 import { Home } from "./js/views/Home.js"
-import {Footer} from "./js/views/footer.js"
-import {headerData} from "./js/views/header.data.js"
-import {Logo} from "./js/views/logo.js"
+
 
 
 window.onload = ()=>{
     AppMain.append(new Home())
-    AppMain.append(new Footer())
-    AppMain.append(new Logo())
-    AppMain.append(new headerData())
+
 }
